01a94c1dcaf6c07806b04a47e2975d05c6f86328
Rafael Rosa <rafaelrd8555@gmail.com>
2024-03-14 12:03:18 -0300

    Slider Buttons add (need fix) and logo

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-let images = document.querySelectorAll('.images img');
+const images = document.querySelectorAll('.images img');
 let index = 0;
 console.log(images.length);
-let text = document.querySelectorAll('.slidesText text');
+const text = document.querySelectorAll('.slidesText text');
 console.log(text.length);
+const buttons =  document.querySelectorAll('.selectButton button ');
+
 
 
 
 function sliderAction() {
  images.forEach(image => image.style.display = 'none');
  images[index].style.display = 'block';
- images[index]
  index = (index + 1) % images.length;
  text.forEach(text => text.style.display = 'none');
  text[index].style.display = 'block';
 
-};
+
+
+
+
+  buttons.forEach( (button, i) => {
+    buttons.addEventLister('click', () => {
+      index = i;
+    })
+   }) 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 sliderAction();
-setInterval(sliderAction, 3000);
+setInterval(sliderAction, 10000);
