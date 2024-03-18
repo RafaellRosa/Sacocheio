9dc3c6aff9b81b073436299c9b4011b6172ac1d3
Rafael Rosa <rafaelrd8555@gmail.com>
2024-03-18 15:33:16 -0300

    Function import fix and minor

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
 import {podcasts} from "./podcasts.js";
+export {clique, sliderAction};
+window.clique = clique; 
 
 const images = document.querySelectorAll('.images img');
 let index = 0;
 const text = document.querySelectorAll('.slidesText text');
 const selectButton = document.querySelectorAll('.buttonstoselect button');
 
-function sliderAction() {
+ function sliderAction() {
   images.forEach(image => image.style.display = 'none');
   images[index].style.display = 'block';
   index = (index) % images.length;
   text.forEach(text => text.style.display = 'none');
   text[index].style.display = 'block';
-
 
     selectButton.forEach(button => {button.classList.remove('selectedButton')});
     selectButton[index].classList.remove('selectbutton');  
@@ -19,16 +20,14 @@ function sliderAction() {
 }
 
 function clique (valor) {
-
   index = valor;
-
   clearInterval(intervalId);
   intervalId = setInterval(() => {
     index = (index + 1) % images.length;
     sliderAction();
   }, 5000);
     sliderAction();
-}
+};
 
 sliderAction(); 
 let intervalId = setInterval(() => {
@@ -36,8 +35,6 @@ let intervalId = setInterval(() => {
   sliderAction(); 
 }, 5000);
 sliderAction();
-
-
 
 podcasts.forEach(podcast =>
  { podcastsContainer.innerHTML += `
